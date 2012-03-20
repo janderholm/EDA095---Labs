3d110dbb4637a366f6c5cb3b51077a8d14954502
Johan Anderholm <johan.anderholm@gmail.com>
2012-03-20 12:35:12 +0100

    sendnig

--- MCAddressServer.java
+++ MCAddressServer.java
@@ -1,27 +1,27 @@
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-
+import java.io.*;
+import java.net.*;
 
 public class MCAddressServer {
-	public static void main(String args[]) throws IOException {
-		MulticastSocket ms = new MulticastSocket(4099);
-		
-		InetAddress ia = InetAddress.getByName("experiment.mcast.net");
-		ms.joinGroup(ia);
-		
-		byte[] buf = new byte[65536];
-		while (true) {
-			DatagramPacket dp = new DatagramPacket(buf, buf.length);
-			ms.receive(dp);
-			String s = new String(dp.getData(), 0, dp.getLength());
-			if ("WHOAREYOU?".equals(s)) {
-				s = InetAddress.getLocalHost().getHostName();
-				dp = new DatagramPacket(s.getBytes(), s.getBytes().length, dp.getSocketAddress());
-				
-			}
-			System.out.println("Received: " + s);
-		}
-	}
+    public static void main(String args[]) throws IOException {
+        MulticastSocket ms = new MulticastSocket(4099);
+        DatagramSocket ds = new DatagramSocket();	
+        InetAddress ia = InetAddress.getByName("experiment.mcast.net");
+        ms.joinGroup(ia);
+	
+        byte[] buf = new byte[65536];
+        while (true) {
+            DatagramPacket dp = new DatagramPacket(buf, buf.length);
+            System.out.println("Receiving.");
+            ms.receive(dp);
+            System.out.println("Received.");
+            String s = new String(dp.getData(), 0, dp.getLength());
+            System.out.println("Received: " + s);
+            if ("WHOAREYOU?".equals(s)) {
+                s = InetAddress.getLocalHost().getHostName();
+                dp = new DatagramPacket(s.getBytes(), s.getBytes().length,
+                       dp.getSocketAddress());
+                ms.send(dp);
+            }
+        }
+    }
 }
