2ed833de311e90696bb973caa8d027177e3d182b
Simon Ekvy <simon.ekvy@gmail.com>
2012-03-25 20:55:03 +0200

    part 1 done

--- EchoTCP1.java
+++ EchoTCP1.java
@@ -1,11 +1,8 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.InetAddress;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.CharBuffer;
 
 public class EchoTCP1 {
 
@@ -15,21 +12,41 @@ public class EchoTCP1 {
 	 */
 	public static void main(String[] args) throws IOException {
 		ServerSocket server = new ServerSocket(30000);
-		while (true) {
-			Socket connection = server.accept();
-			System.out.println(connection.getInetAddress().toString());
-		
-			InputStream inStream = connection.getInputStream();
-			
-			while(inStream.read()){
-				
+		int buffSize = 500;
+		byte[] buff    = new byte[500];
+		int bytesRead   = 0;
+		int result;
+		Socket connection;
+		OutputStream out = null;
+		InputStream inStream;
+		while(true){
+			connection = server.accept();
+			System.out.println("Connection from: " + connection.getInetAddress().toString());
+			inStream = connection.getInputStream();
+			out = connection.getOutputStream();
+			bytesRead = 0; //reset counter
+			buffSize = 500; //reset size
+			while (bytesRead < buffSize) {
+				result = inStream.read(buff, bytesRead, buffSize - bytesRead);
+				if (result == -1) break;
+				bytesRead += result;
+				if(bytesRead >= buffSize){
+					byte[] tbuff = buff;
+					buffSize += bytesRead;
+					buff = new byte[buffSize];
+			        System.arraycopy(tbuff, 0, buff, 0, bytesRead);
+				}
+		        //System.out.println(bytesRead);
 			}
-			
-			OutputStreamWriter out = new OutputStreamWriter(
-					connection.getOutputStream());
-			out.write("You've connected to this server. Bye-bye now.\r\n");
+			System.out.println(new String(buff,0,bytesRead));
+			out.write(buff,0,bytesRead);
+			out.flush();
 			connection.close();
 		}
+		
+		/*OutputStreamWriter out = new OutputStreamWriter(
+				connection.getOutputStream());
+		out.write("You've connected to this server. Bye-bye now.\r\n");*/
 	}
 
 }
