970deff92c2ddbee3962900242aa93119257fc37
Johan Anderholm <johan.anderholm@gmail.com>
2012-04-24 00:47:01 +0200

    Awesome cake

--- Crawler.scala
+++ Crawler.scala
@@ -5,6 +5,7 @@ import scala.actors.Actor
 import scala.util.matching.Regex
 import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
 import java.io.IOException
+import scala.collection.immutable.TreeSet
 
 object TagSoupXmlLoader {
   private val factory = new SAXFactoryImpl()
@@ -18,31 +19,47 @@ object TagSoupXmlLoader {
  */
 
 case class MailTo(address: String)
-case class HTTPLink(url: String)
+case class HTTPLink(address: String)
+case class Link(address: String)
+case class Done()
 
 class CrawlControl extends Actor {
+   
+  def printData(mails: List[String], links: List[String]) {
+    println("Finished gathering: " + links.length + " links")
+    links foreach println
+    mails foreach println
+  }
+  
   def act() {
-    def loop(mails: List[String], links: List[String]) {
+    def loop(running: Int, seen: TreeSet[String], mails: List[String], links: List[String]) {
       react {
-        case MailTo(address) ⇒
-          if (!(mails contains address))
-            loop(address :: mails, links)
-          else
-            loop(mails, links)
+        case MailTo(address)   ⇒ loop(running, seen, address :: mails, links)
 
         case HTTPLink(address) ⇒ {
-          if (links.length < 1000) {
-            if (!(links contains address)) {
-              new Crawler(address, this).start()
-              println(links.length)
-              loop(mails, address :: links)
-            }
-            loop(mails, links)
-          } else {
-            println("Finished! " + links.length)
-            links foreach println
-            mails foreach println
+          if ((links.length % 100) == 0) {
+            println("Links gathered: " + links.length)
           }
+          loop(running, seen, mails, address :: links)
+        }
+
+        case Link(address) ⇒ {
+          if (links.length < 1000) {
+            if (!(seen contains address)) {
+              new Crawler(address, this).start()
+              loop(running + 1, seen + address, mails, links)
+            }
+            loop(running, seen, mails, links)
+          } else {
+            printData(mails, links)
+          }
+        }
+
+        case Done() ⇒ {
+          if (running > 1)
+            loop(running - 1, seen, mails, links)
+          else
+        	printData(mails, links)
         }
 
         case _ ⇒ {
@@ -51,41 +68,38 @@ class CrawlControl extends Actor {
 
       }
     }
-    loop(List(), List())
+    loop(0, TreeSet(), List(), List())
   }
 }
 
 class Crawler(val link: String, control: Actor) extends Actor {
   def act() {
-
+  
     val url = new URL(link)
     try {
       val connection = url.openConnection()
-      connection setConnectTimeout 1000
-      //connection.setReadTimeout(2000)
-
+      connection setConnectTimeout 500
+      connection setReadTimeout 1000
+      
+      if (link startsWith "mailto:") {
+        control ! MailTo(link)
+      } else if(connection.getContentType contains "text/html") {
+        control ! HTTPLink(link)
+      } else {
+        return
+      }
+     
       val html = TagSoupXmlLoader.htmlParser load connection.getInputStream
 
       for (link ← (html \\ "@href" ++ html \\ "@src")) {
-        val address = link.text
-        
-        if (address.startsWith("mailto:")) {
-          control ! MailTo(address)
-        } else {
-          val newUrl = new URL(url, address)
-          val newConnection = newUrl.openConnection()
-          newConnection setConnectTimeout 1000
-          
-          println(newUrl.toString())
-
-          if (newConnection.getContentType contains "text/html") {
-            control ! HTTPLink(newUrl.toString())
-          }
-        }
+        val address = new URL(url, link.text)
+        control ! Link(address.toString)
       }
     } catch {
       case ioe: IOException ⇒ {}
-      case e: Exception => {println(url.toString())}
+      case e: Exception     ⇒ {}//e.printStackTrace();  println("This one broke: " + url.toString())}
+    } finally {
+      control ! Done()
     }
   }
 }
@@ -96,8 +110,7 @@ object Main extends App {
   control.start
 
   Actor.actor {
-    val future = control !! HTTPLink("https://github.com/")
-    future()
+    control ! Link("http://cs.lth.se/kurs/eda095")
   }
 
 }
